19e7d669f749c87b4d42acd52d34ca0af9c6da00
Butta <inan1deniz@gmail.com>
2020-09-19 13:08:13 +0200

    activity view

--- frontend/src/pages/token.js
+++ frontend/src/pages/token.js
@@ -7,44 +7,87 @@ export default function activity() {
     <main id="site-main" role="main" className="app-content">
       <div className="container" style={{
       padding: '0rem',
-    }}></div>
+    }}>
+        <div className="gallery-grid" style={{
+        //_poapapp.scss media 
+      }}></div>
+    
+      <div  style={{display: "flex", justifyContent: "space-between", margin:"0rem 0",}}>
+       <div style={{flex: "3"}}> <TokenCard/> </div> 
+       <div style={{display: "flex", flexDirection: "column", flex: "7", justifyContent: "center"}}> 
+     
+       <h4> Description </h4>
+       <h4> Virtual Event </h4>
+       <h4> Country </h4>
+       <h4> Start date </h4>
+       <h4> End date </h4>
+       <h4> Website </h4>
+       </div>
 
-<div  style={{display: "flex", alignItems: "center", margin:"0rem 0",}}>
-        test
+
+       
   </div>
 
-  <div className="gallery-grid" style={{
-          //_poapapp.scss media 
-      }}>
+  <div  style={{display: "flex", alignItems: "center", margin:"2rem 0",}}>
+        <table className="activityTable" style={{width: "100%" ,border: "none"}}>
+            <tr>
+              <th>#</th>
+              <th>Owner</th>
+              <th>Claim date</th>
+              <th>Transfer count</th>
+            
+            </tr>
+            <tr> 
+              <td>1</td>
+              <td>ABC </td>
+              <td>0xb4367dc4d2</td>
+              <td> Inan</td>
+            </tr>
+            <tr> 
+              <td>2</td>
+              <td>ABC POAP</td>
+              <td>0xb4367dc4de</td>
+              <td>0xb4367dc4d2</td>
 
-        <Cards/>
-      </div>
+            </tr>
+            <tr> 
+              <td>3</td>
+              <td>ABC PP3</td>
+              <td>0xb4367dc4de</td>
+              <td>Stefan.eth</td>
+
+            </tr>
+            <tr> 
+              <td>4</td>
+              <td>ABC 1</td>
+              <td>Max.eth</td>
+              <td>Alexander.eth</td>
+   
+            </tr>
+        </table>
+
+</div>
+
+
+
+  </div>
     </main>
   )
 }
 
 
-function Cards() {
-    var amount = 1;
-    var cards = []
-    for (let index = 0; index < amount ; index++) {
-      cards.push(<TokenCard/>)
-    }
-    return cards
-  }
-  
   function TokenCard() {
     return (
         <div style={{
           // border: 'black solid 1px',
           borderRadius: '.5rem',
-          width: '200px',
-          height: '200px',
+          width: '250px',
+          height: '250px',
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'space-around',
           alignItems: 'center',
-          boxShadow: '0 5px 16px rgba(100,100,100, 0.3)',
+          //boxShadow: '0 5px 16px rgba(100,100,100, 0.3)',
           padding: '1rem 0rem',
         }}>
           <div style={{
@@ -53,8 +96,8 @@ function Cards() {
             justifyContent: 'center',
             alignItems: 'center',
             display: 'flex',
-            width: '75px',
-            height: '75px',
+            width: '175px',
+            height: '175px',
           }}>
             <img style={{
               width: 'auto',
