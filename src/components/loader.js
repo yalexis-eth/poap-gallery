5168acda5ff5d85b4772923fe4f67f338f6feebe
sebas favaron <sfavaron@balanz.com>
2021-06-17 11:50:54 -0300

    Add support for lottie animations
    + added loading animation

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Lottie from 'react-lottie-player'
+import loadingAnimation from '../assets/animations/loading_animation.json'
 
 export default function () {
   return (
-    <div className="spinner">
-      <div className="cube1"></div>
-      <div className="cube2"></div>
-    </div>
+    <Lottie
+      loop
+      animationData={loadingAnimation}
+      play
+      className='spinner'
+      style={{ width: 150, height: 150 }}
+    />
   )
 }
